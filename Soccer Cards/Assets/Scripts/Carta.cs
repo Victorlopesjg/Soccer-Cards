1b5a2eb1c95b4ed4e2b1bc50aca26e04a0d4179c
Victor Oliveira <victorlopesjg@gmail.com>
2016-03-03 20:04:45 -0300

    Atualização 001

---
+++ Soccer Cards/Assets/Scripts/Carta.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Carta : MonoBehaviour {
+
+	public int pontosAtaque;
+	public int pontosDefesa;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1))
+            Destroy(gameObject);
+	}
+
+}
