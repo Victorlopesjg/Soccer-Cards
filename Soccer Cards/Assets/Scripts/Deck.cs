dfea7676f914d325a86bdd23af2bba7147bed1bc
Adelino Neto Segundo <adelinosegundo@gmail.com>
2016-03-03 22:02:11 -0300

    added deck and game controller

---
+++ Soccer Cards/Assets/Scripts/Deck.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour {
+	//PUBLIC ATTRS
+	public List<GameObject> cardsKinds;
+	public int cardsCount;
+
+	//PRIVATE
+	private List<GameObject> deck;
+	private GameObject selectedCard;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("DECK STARTED");
+		deck = new List<GameObject> ();
+		for (int i = 0; i < cardsCount; i++){
+			GameObject card = Instantiate (cardsKinds [Random.Range (0, cardsKinds.Count)]);
+			deck.Add(card);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SelectCard(int index){
+		selectedCard = deck [index];
+		deck.Remove (selectedCard);
+	}
+
+	public List<GameObject> CardsInHand(){
+		List<GameObject> cardsInHand = new List<GameObject> ();
+		if (gameObject) cardsInHand.AddRange( ((Deck)gameObject.GetComponent(typeof(Deck))).deck.GetRange(0, 5) );
+		return cardsInHand;
+	}
+}
---
+++ Soccer Cards/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject deck;
+
+	private GameObject playerDeck;
+	private GameObject aiDeck;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (startDecksCoroutine());
+	}
+
+	IEnumerator startDecksCoroutine(){
+		StartDecks ();
+		yield return new WaitForSeconds(1);
+		LogDecks ();
+	}
+
+	void StartDecks(){
+		playerDeck = Instantiate (deck);
+	}
+
+	void LogDecks(){
+		Deck playerDeckScript = (Deck) playerDeck.GetComponent (typeof(Deck));
+		Debug.Log (playerDeckScript.CardsInHand()[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
